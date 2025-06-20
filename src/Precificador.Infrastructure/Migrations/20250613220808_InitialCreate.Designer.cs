537ab8bc74850ce62e8a2be29eee4bdb9e88a359
Anderson Marzola <ajmarzola@gmail.com>
2025-06-20 13:32:51 -0300

    Atualização do Framework

--- src/Precificador.Infrastructure/Migrations/20250613220808_InitialCreate.Designer.cs
+++ src/Precificador.Infrastructure/Migrations/20250613220808_InitialCreate.Designer.cs
@@ -20,7 +20,7 @@ namespace Precificador.Infrastructure.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "9.0.5")
+                .HasAnnotation("ProductVersion", "9.0.6")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
